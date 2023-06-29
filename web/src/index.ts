f0b681b2c6b92c3678b724c3857bbca91d30c21f
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-06-29 09:41:34 -0300

    fix: dev env backend routing

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import proxy from 'express-http-proxy';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -6,10 +7,14 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express+TypeScript Server');
+app.route('/api/latest/*')
+    .get(proxy('http://127.0.0.1:8787/', { parseReqBody: false }))
+    .head(proxy('http://127.0.0.1:8787/', { parseReqBody: false }))
+    .post(proxy('http://127.0.0.1:8787/'))
+app.use('/*', (req: Request, res: Response) => {
+    res.send('Express+TypeScript Server');
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
