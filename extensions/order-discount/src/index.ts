5d1fddd95729b3c865c28c4022f72c056b2e7503
Ricardo Gonçalves <ricardo@brgweb.com.br>
2023-08-04 10:58:24 -0300

    fix: order discount works

--- extensions/order-discount/src/index.ts
+++ extensions/order-discount/src/index.ts
@@ -18,15 +18,28 @@ const EMPTY_DISCOUNT: FunctionResult = {
 
 const isDiscountAllowed = (input: InputQuery) => {
     const discountAllowed: string | undefined = input.cart.buyerIdentity?.customer?.discountAllowed?.value
+    if(discountAllowed === undefined){
+        return false;
+    }
+    const discountAllowedList: Array<string> = JSON.parse(discountAllowed)
+    if( discountAllowedList.length === 0 ){
+        return false;
+    }
+    console.log(`discountAllowed ${discountAllowed}`);
     const tid: string = input.discountNode.tid!.value
-    return !discountAllowed || JSON.parse(discountAllowed).indexOf(tid) === -1
+    console.log(`tid ${tid}`);
+    console.log(discountAllowed);
+    console.log(JSON.parse(discountAllowed!).indexOf(tid));
+    return JSON.parse(discountAllowed).indexOf(tid) !== -1
 }
 
 const isAlreadyUsed = (input: InputQuery, discountMeta: ShopifyDiscountMeta) => {
     const tid: string = input.discountNode.tid!.value
+    console.log(`tid ${tid}`);
     if(  discountMeta.onePerUser ){
         const discounUsed = input.cart.buyerIdentity?.customer?.discountUsed?.value ?? '[]'
-        return JSON.parse(discounUsed).indexOf(tid) === -1
+        console.log(`discountUsed ${discounUsed}`);
+        return JSON.parse(discounUsed).indexOf(tid) !== -1
     }
     return false
 }
@@ -66,12 +79,17 @@ const setConditions = (input: InputQuery, discountMeta: ShopifyDiscountMeta) => 
 export default (input: InputQuery): FunctionResult => {
     try{
         const discountMeta: ShopifyDiscountMeta = JSON.parse(input.discountNode.discount_meta!.value)
+        console.log(JSON.stringify(discountMeta));
         if(!isDiscountAllowed(input) || isAlreadyUsed(input, discountMeta)){
             return EMPTY_DISCOUNT
         }
+        console.log('conditions');
         const conditions: Array<Condition> = setConditions(input, discountMeta)
+        console.log('targets');
         const targets: Array<Target> = [{ orderSubtotal: { excludedVariantIds: [] } as OrderSubtotalTarget }]
+        console.log('value');
         const value: Value = setDiscountValue(input, discountMeta)
+        console.log('discount');
         const discount: Discount = {
             conditions,
             targets,
